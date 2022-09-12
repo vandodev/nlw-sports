baf92c59af5aeeb5975f60fac571934d648e32e7
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-12 18:50:01 -0300

    :video_game: :rocket: :video_game: web - Configurando tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
-
+import './styles/main.css'
 function App() {
-  return <h1>Olá mundo</h1>
+  return (
+    <div className='w-8 h-8 bg-violet-500'></div>
+  )
 }
 
-export default App
+export default App 
