1ef4c529f63146bc2847a5379cb95c540563ed2b
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-12 19:24:36 -0300

    :video_game: :rocket: :video_game: web - Textos com gradient com tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 ">
       <img src={logoImage} alt="Next Level Week Exports Logo" />
+      <h1 className="text-6xl text-white font-black mt-16 bg-galaxy-gradient bg-clip-text">
+                Seu <span className="text-transparent">duo</span> está aqui
+      </h1>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
        galaxy: "url('/background-galaxy.png')",
+                'galaxy-gradient':
+                    'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+                'game-gradient':
+                    'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
