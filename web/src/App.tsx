d27c68a397412389dbf00ad54b66a8572a355fe2
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-12 21:32:02 -0300

    :video_game: :rocket: :video_game: web - Área chamada publicar anúncio

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css'
 import logoImage from './assets/logo-nlw-esports.svg'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 ">
@@ -8,7 +9,7 @@ function App() {
                 Seu <span className="text-transparent">duo</span> está aqui
       </h1>
 
-        <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="grid grid-cols-6 gap-6 mt-16">
               <a href='' className='relative rounded-lg overflow-hidden'>
                 <img src='/game-1.png' alt=''/> 
                 <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -58,7 +59,21 @@ function App() {
               </a> 
 
               
+      </div>
+
+      <div className='pt-1 mt-8 bg-galaxy-gradient self-stretch rounded-lg overflow-hidden '>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center  '>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu Duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar</span>
+          </div>
+
+          <button className='py-3 px-4 flex items-center gap-3 bg-violet-500 hover:bg-violet-600 text-white rounded'>
+            <MagnifyingGlassPlus size={24}/>
+            Publocar anúncio
+          </button>
         </div>
+      </div>
     </div>
   )
 }
