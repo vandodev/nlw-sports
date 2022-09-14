1cb383921b6bd493b182a0ce5fa479161bebff7c
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-13 22:16:47 -0300

    :video_game: :rocket: :video_game: Mobile - Aplicando ImageBackground

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,22 @@
+import { ImageBackground } from 'react-native';
+
+import { styles } from './styles';
+
+import backgroundImage from '../../assets/background-galaxy.png';
+
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground
+      source={backgroundImage}
+      defaultSource={backgroundImage}
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
