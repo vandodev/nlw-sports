23ed3ad7d7e1897f564840ec0900fca5386b5d29
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-13 23:43:25 -0300

    :video_game: :rocket: :video_game: Mobile - Component Heading

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,16 @@
 import { View, Image } from "react-native";
 import { styles } from './styles';
 import logoImage from '../../assets/logo-nlw-esports.png';
+import { Heading } from "../../components/Heading";
 
 export function Home() {
     return(
         <View style={styles.container}>
             <Image source={logoImage} style={styles.logo}></Image>
+            <Heading
+              title='Encontra o teu duo!'
+              subtitle='Seleciona o jogo que deseja jogar...'
+            />
         </View>
     )
 }
