27c064dcdf90aa0935402492898732b725cd7794
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-16 14:46:39 -0300

    :video_game: :rocket: :video_game: Web - Criando component GameCard

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/main.css'
 import logoImage from './assets/logo-nlw-esports.svg'
 import {MagnifyingGlassPlus} from 'phosphor-react'
+import { GameCard } from './components/GameCard'
 function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 ">
@@ -10,55 +11,12 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-1.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Game of legends</strong>
-                  <span className='text-zinc-300 text-sm block '>4 anúncios</span>
-                </div>
-              </a>  
-
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-2.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Dota</strong>
-                  <span className='text-zinc-300 text-sm block '>3 anúncios</span>
-                </div>
-              </a>  
-
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-3.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Counter Strike</strong>
-                  <span className='text-zinc-300 text-sm block '>6 anúncios</span>
-                </div>
-              </a> 
-
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-4.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Apex legends</strong>
-                  <span className='text-zinc-300 text-sm block '>4 anúncios</span>
-                </div>
-              </a> 
-
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-5.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Game of legends</strong>
-                  <span className='text-zinc-300 text-sm block '>4 anúncios</span>
-                </div>
-              </a> 
-
-              <a href='' className='relative rounded-lg overflow-hidden'>
-                <img src='/game-6.png' alt=''/> 
-                <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-                  <strong className='font-bold text-white block'>Game of legends</strong>
-                  <span className='text-zinc-300 text-sm block '>4 anúncios</span>
-                </div>
-              </a> 
-
-              
+          <GameCard bannerUrl='/game-1.png' title='game of legends' adsCount={5}/>
+          <GameCard bannerUrl='/game-2.png' title='game of legends' adsCount={2}/> 
+          <GameCard bannerUrl='/game-3.png' title='game of legends' adsCount={10}/> 
+          <GameCard bannerUrl='/game-4.png' title='game of legends' adsCount={7}/> 
+          <GameCard bannerUrl='/game-5.png' title='game of legends' adsCount={9}/> 
+          <GameCard bannerUrl='/game-6.png' title='game of legends' adsCount={5}/>               
       </div>
 
       <div className='pt-1 mt-8 bg-galaxy-gradient self-stretch rounded-lg overflow-hidden '>
