7c550b533914ced833d879fdc3adf638c62ebb85
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-17 20:46:14 -0300

    :video_game: :rocket: :video_game: Mobile - Recuperando dados passado por parámetro

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,8 +1,13 @@
 import {  Text } from 'react-native';
+import {useRoute} from "@react-navigation/native"
 import {SafeAreaView} from "react-native-safe-area-context"
 import { Background } from '../../components/Background';
 import { styles } from './styles';
+
 export function Game() {
+    const route = useRoute();
+    const game = route.params;
+    console.log(game)
     return(
         <Background>
             <SafeAreaView style={styles.container}>
