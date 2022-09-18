de734e556660024f3e435a0a735f66e2c3ea1916
Evandro oliveira <vandoaparecido@hotmail.com>
2022-09-18 19:39:15 -0300

    :video_game: :rocket: :video_game: Mobile - Estilizando tela de detalhe de game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,17 +1,49 @@
-import {  Text } from 'react-native';
-import {useRoute} from "@react-navigation/native"
+import {  Image, Text, TouchableOpacity, View } from 'react-native';
+import {useNavigation,useRoute} from "@react-navigation/native";
 import {SafeAreaView} from "react-native-safe-area-context"
 import { Background } from '../../components/Background';
 import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
+import { THEME } from '../../theme';
+import { Entypo } from '@expo/vector-icons';
+import logoImage from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Game() {
     const route = useRoute();
-    const game = route.params;
+    const navigation = useNavigation();
+    const game = route.params as GameParams;
     console.log(game)
+
+    const handleGoBack = () => {
+        navigation.goBack();
+    };
+
     return(
         <Background>
             <SafeAreaView style={styles.container}>
-                <Text>Game</Text>
+                 <View style={styles.header}>
+                    <TouchableOpacity onPress={() => handleGoBack()}>
+                        <Entypo
+                            name='chevron-thin-left'
+                            color={THEME.COLORS.CAPTION_300}
+                            size={20}
+                         />
+                    </TouchableOpacity>
+                    <Image source={logoImage} style={styles.logo}></Image>
+                    <View style={styles.right}/>
+                </View>
+
+                <Image
+                    source={{ uri: game.bannerUrl }}
+                    style={styles.cover}
+                    resizeMode='cover'
+                />
+
+                <Heading
+                  title={game.title}
+                  subtitle={'Conecta-te e começa a jogar!'}
+                /> 
             </SafeAreaView>
         </Background>
     );
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -19,7 +19,7 @@ export function Home() {
     }
 
     useEffect(() =>{
-        fetch('http://192.168.1.6:3333/games')
+        fetch('http://192.168.1.4:3333/games')
         .then(response => response.json())
         .then(data =>{
         // console.log(data)
